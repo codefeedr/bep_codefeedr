d23a69952b411ee35b8040ee64a64b300ab25ac7
Joris Quist <Jorisquist@gmail.com>
2018-05-22 16:58:11 +0200

    Implemented TravisBuildCollecter

--- src/main/scala/org/codefeedr/plugins/travis/TravisBuildCollector.scala
+++ src/main/scala/org/codefeedr/plugins/travis/TravisBuildCollector.scala
@@ -1,5 +1,110 @@
 package org.codefeedr.plugins.travis
 
-class TravisBuildCollecter() {
+import java.time.LocalDateTime
+
+import org.apache.flink.runtime.concurrent.Executors
+
+import scala.concurrent.{ExecutionContext, Future}
+import ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+object TravisBuildCollector {
+
+  def main(args: Array[String]): Unit = {
+//    implicit lazy val executor: ExecutionContext = ExecutionContext.fromExecutor(Executors.directExecutor())
+
+    val collector = new TravisBuildCollector("joskuijpers", "bep_codefeedr", "develop", "bb5cc7e5a19a84d71d627d202d44ea81598d9a68", LocalDateTime.MIN)
+
+    val result: Future[Option[TravisBuild]] = collector.requestFinishedBuild()
+    result.onComplete {
+      case Success(Some(x)) => println("Succeeded", x.commit.message.replace('\n', ' '))
+      case Success(None) => println("Build not found")
+      case Failure(e) => e.printStackTrace()
+    }
+
+    while(true) Thread.sleep(1000)
+
+  }
+}
+
+
+//TODO only give GithubPushEvent as constructor argument
+class TravisBuildCollector(repoOwner: String, repoName: String, branchName: String, pushCommitSha: String, pushDate: LocalDateTime) {
+  private val travis: TravisService = new TravisService()
+
+  private var willNotBuild: Boolean = false
+  private var minimumCreationDate: LocalDateTime = pushDate
+  private var build: Option[TravisBuild] = None
+
+  def requestFinishedBuild(): Future[Option[TravisBuild]] = Future {
+    while (!isReady) {
+      println("Requesting build")
+      build = requestBuild()
+      println("Status of the build: " + (if(build.nonEmpty) build.get.state else "not found yet"))
+      if (!isReady) {
+        println("Not ready yet now sleeping for 10 seconds")
+        Thread.sleep(10000)
+      }
+    }
+    println("Build is found and is finished. Now returning...")
+    build
+  }
+
+  private def isReady: Boolean = {
+    if (willNotBuild) {
+      true
+    }
+    else if (build.nonEmpty) {
+      val state = build.get.state
+      state == "passed" || state == "failed"
+    }
+    else {
+      false
+    }
+  }
+
+  private def requestBuild(): Option[TravisBuild] = build match {
+    case None => requestUnknownBuild()
+    case Some(_) => requestKnownBuild()
+  }
+
+  def requestKnownBuild(): Option[TravisBuild] = {
+    assert(build.nonEmpty)
+    Some(travis.getBuild(build.get.id))
+  }
+
+  def requestUnknownBuild(): Option[TravisBuild] = {
+
+    var newestBuildDate: Option[LocalDateTime] = None
+
+    var builds: TravisBuilds = new TravisService().getTravisBuilds(repoOwner, repoName, branchName, limit = 5)
+    if (builds.builds.nonEmpty) {
+      if(!builds.builds.head.repository.active.orElse(Some(false)).get) {
+        willNotBuild = true
+        return None
+      }
+      newestBuildDate = builds.builds.head.started_at
+    }
+
+    do {
+      val buildIterator = builds.builds.iterator
+
+      while (buildIterator.hasNext) {
+        val x = buildIterator.next()
+        if (x.started_at.getOrElse(LocalDateTime.MAX).isBefore(minimumCreationDate)) {
+          return None
+        }
+        else if (x.commit.sha == pushCommitSha) {
+          return Some(x)
+        }
+      }
+
+      val offset = if (builds == null ) 0 else builds.`@pagination`.next.offset
+      builds = new TravisService().getTravisBuilds(repoOwner, repoName, branchName, offset, limit = 5)
+
+    } while (!builds.`@pagination`.is_last)
+
+    None
+  }
 
 }
--- src/main/scala/org/codefeedr/plugins/travis/TravisService.scala
+++ src/main/scala/org/codefeedr/plugins/travis/TravisService.scala
@@ -1,16 +1,24 @@
 package org.codefeedr.plugins.travis
 
+import java.time.LocalDateTime
+
 import org.codefeedr.keymanager.{KeyManager, StaticKeyManager}
 import org.codefeedr.utilities.Http
 import org.json4s.DefaultFormats
 import org.json4s.ext.JavaTimeSerializers
 import org.json4s.jackson.JsonMethods.parse
 
-object TravisService {
-  def main(args: Array[String]): Unit = {
-    new TravisService().getTravisBuilds("joskuijpers%2Fbep_codefeedr").builds.foreach(x => println(x.commit.sha, x.number, x.started_at, x.state))
-  }
-}
+//object TravisService {
+//  def main(args: Array[String]): Unit = {
+////    new TravisService().getTravisBuilds("owner", repoName = "branch").builds.foreach(x => println(x.commit.sha, x.number, x.started_at, x.state))
+//    println(new TravisService().getBuild(
+//      "joskuijpers",
+//      "bep_codefeedr",
+//      "develop",
+//      "bb5cc7e5a19a84d71d627d202d44ea81598d9a68",
+//      LocalDateTime.MIN))
+//  }
+//}
 
 class TravisService() {
 
@@ -26,18 +34,61 @@ class TravisService() {
     isActive
   }
 
-  def getTravisBuilds(slug: String): TravisBuilds = {
-    val responseBody = getTravisResource("/repo/" + slug + "/builds?sort_by=started_at:desc&state=started,failed,passed")
+  def getTravisBuilds(owner: String, repoName: String, branch: String = "", offset: Int = 0, limit: Int = 25): TravisBuilds = {
+    getTravisBuildsWithSlug(owner + "%2F" + repoName, branch, offset, limit)
+  }
+
+  def getTravisBuildsWithSlug(slug: String, branch: String = "", offset: Int = 0, limit: Int = 25): TravisBuilds = {
+
+    println("\tGetting travis builds " + offset + " until " + (offset + limit))
+
+    val url = "/repo/" + slug + "/builds" +
+      (if (branch.nonEmpty) "?branch.name=" + branch else "?") +
+      "&sort_by=started_at" +
+//      "&state=created,started,failed,passed" +
+      "&include=build.repository" +
+      "&offset=" + offset +
+      "&limit=" + limit
+
+    val responseBody = getTravisResource(url)
     val json =  parse(responseBody)
     val builds = json.extract[TravisBuilds]
     builds
   }
 
-  def getTravisBuilds(slug: String, branch: String): TravisBuilds = {
-    val responseBody = getTravisResource("/repo/" + slug + "/builds?branch.name=" + branch + "&sort_by=started_at&state=started,failed,passed")
+//  def getBuild(owner: String, repoName: String, branch: String, pushCommitSha: String, afterDate: LocalDateTime, cachedBuilds: Option[TravisBuilds] = None): Option[TravisBuild] = {
+//
+//    var builds: TravisBuilds = cachedBuilds.orNull
+//
+//    do {
+//      val offset = if (builds == null ) 0 else builds.`@pagination`.next.offset
+//      println("offset", offset)
+//      builds = getTravisBuilds(owner, repoName, branch, offset, limit = 5)
+//      val buildIterator = builds.builds.iterator
+//
+//      while (buildIterator.hasNext) {
+//        val x = buildIterator.next()
+//        println(x.number)
+//        if (x.commit.sha == pushCommitSha) {
+//          return Some(x)
+//        }
+//        else if (x.started_at.isBefore(afterDate)) {
+//          return None
+//        }
+//      }
+//
+//    } while (!builds.`@pagination`.is_last)
+//
+//    None
+//  }
+
+  def getBuild(buildID: Int): TravisBuild = {
+    val url = "/build/" + buildID
+
+    val responseBody = getTravisResource(url)
     val json =  parse(responseBody)
-    val builds = json.extract[TravisBuilds]
-    builds
+    val build = json.extract[TravisBuild]
+    build
   }
 
   private def getTravisResource(endpoint: String): String = {
